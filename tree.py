e5e3ae8023c33de5497127a6bd00f31fabe97fc3
Letao Jiang <letaoj@gmail.com>
2021-12-21 18:58:55 -0800

    Adding tree skeleton and placeholder tree class

---
+++ tree.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+tree = list(open('tree1.txt').read().rstrip())
+
+class Tree:
+    def colored_dot(self):
+        pass
+
+    def lights(self):
+        pass
+
+    def color_dot(self):
+       pass 
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
